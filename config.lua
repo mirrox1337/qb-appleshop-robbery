1ef5c73c9e359779c281f1214c30f4d6746608dc
Samuel Nicol <99494967+Github-Samuel@users.noreply.github.com>
2022-03-10 16:15:29 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,125 @@
+Config = {}
+
+Config.Locations = {
+    ["main"] = {
+        x = 0.0,
+        y = 0.0,
+        z = 0.0,
+    },
+    ["thermite"] = {
+        x =  124.56,
+        y = -242.21,
+        z = 53.35,
+        isDone = false,
+        isBusy = false,
+    },
+    ["takeables"] = {
+        [1] = {
+            x = 158.78,
+            y = -225.67,
+            z = 54.42,
+            isDone = false,
+            isBusy = false,
+            reward =  {name = "tablet", amount = 4},
+        },
+        [2] = {
+            x = 155.52,
+            y = -224.66,
+            z =  54.42,
+            isDone = false,
+            isBusy = false,
+            reward =  {name = "iphone", amount = 6},
+        },
+        [3] = {
+            x =  153.87,
+            y = -229.21,
+            z = 54.42,
+            isDone = false,
+            isBusy = false,
+            reward =  {name = "laptop", amount = 3},
+        },
+        [4] = {
+            x = 150.31,
+            y = -222.43,
+            z = 54.42,
+            isDone = false,
+            isBusy = false,
+            reward =  {name = "samsungphone", amount = 6},
+        },
+        [5] = {
+            x = 157.06,
+            y = -224.80,
+            z = 54.42,
+            isDone = false,
+            isBusy = false,
+            reward =  {name = "tablet", amount = 4},
+        },
+        [6] = {
+            x = 155.44,
+            y = -229.41,
+            z = 54.42,
+            isDone = false,
+            isBusy = false,
+            reward =  {name = "laptop", amount = 3},
+        },
+        [7] = {
+            x =  153.82,
+            y = -223.89,
+            z = 54.42,
+            isDone = false,
+            isBusy = false,
+            reward =  {name = "iphone", amount = 4},
+        },
+        [8] = {
+            x =  151.97,
+            y = -223.30,
+            z = 54.42,
+            isDone = false,
+            isBusy = false,
+            reward =  {name = "iphone", amount = 6},
+        },
+        [9] = {
+            x = 152.19,
+            y = -228.28,
+            z = 54.42,
+            isDone = false,
+            isBusy = false,
+            reward =  {name = "laptop", amount = 4},
+        },
+        [10] = {
+            x = 157.04,
+            y = -230.37,
+            z = 54.42,
+            isDone = false,
+            isBusy = false,
+            reward =  {name = "laptop", amount = 3},
+        },
+        [11] = {
+            x = 150.36,
+            y = -227.75,
+            z = 54.42,
+            isDone = false,
+            isBusy = false,
+            reward =  {name = "imac", amount = 2},
+        },
+        [12] = {
+            x =  147.15,
+            y = -226.74,
+            z = 54.42,
+            isDone = false,
+            isBusy = false,
+            reward =  {name = "imac", amount = 2},
+        },
+    },
+     ["safe"] = {
+         x = -625.37,
+         y = -283.74,
+         z = 35.57,
+         isDone = false,
+         isBusy = false,
+     }
+}
+
+Config.MaleNoHandshoes = {[0] = true,[1] = true,[2] = true,[3] = true,[4] = true,[5] = true,[6] = true,[7] = true,[8] = true,[9] = true,[10] = true,[11] = true,[12] = true,[13] = true,[14] = true,[15] = true,[18] = true,[26] = true,[52] = true,[53] = true,[54] = true,[55] = true,[56] = true,[57] = true,[58] = true,[59] = true,[60] = true,[61] = true,[62] = true,[112] = true,[113] = true,[114] = true,[118] = true,[125] = true,[132] = true,}
+
+Config.FemaleNoHandshoes = {[0] = true,[1] = true,[2] = true,[3] = true,[4] = true,[5] = true,[6] = true,[7] = true,[8] = true,[9] = true,[10] = true,[11] = true,[12] = true,[13] = true,[14] = true,[15] = true,[19] = true,[59] = true,[60] = true,[61] = true,[62] = true,[63] = true,[64] = true,[65] = true,[66] = true,[67] = true,[68] = true,[69] = true,[70] = true,[71] = true,[129] = true,[130] = true,[131] = true,[135] = true,[142] = true,[149] = true,[153] = true,[157] = true,[161] = true,[165] = true,}
